25e3c5c615ebc4771fe2516f00b22f6801839ae9
matheustkaczyk <matheus111904@hotmail.com>
2022-02-09 19:03:34 -0300

    Estruturando o componente header, adicionado a logo svg, input de busca e botoes provisorios de minha conta e carrinho

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-      teste
+      <Main />
     </div>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 const Main = () => {
   return(
     <div>
-      main
+      <Header />
     </div>
   )
 };

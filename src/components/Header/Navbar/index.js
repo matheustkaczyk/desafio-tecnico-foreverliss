63413223a306433d2d533a48c38cc41a7d6eef02
matheustkaczyk <matheus111904@hotmail.com>
2022-02-09 19:07:08 -0300

    Estruturado navbar, adicionado background color tambem

---
+++ src/components/Header/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <navbar>
+      <a>PERFUME INFANTIL</a>
+      <a>SABONETES</a>
+      <a>PÓS-BANHO</a>
+      <a>ASSADURAS</a>
+      <a>HIDRATANTES</a>
+      <a>LENÇOS UMEDECIDOS</a>
+    </navbar>
+  )
+};
+
+export default Navbar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Navbar from '../components/Header/Navbar';
 
 const Main = () => {
   return(
     <div>
       <Header />
+      <Navbar />
     </div>
   )
 };

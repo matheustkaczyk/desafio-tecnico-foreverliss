233e95ec424cbd1fee53c8e1eb9e7aba0e5ad591
matheustkaczyk <matheus111904@hotmail.com>
2022-02-10 14:30:16 -0300

    Estilizacao footer concluido, ainda esta imagem estatica ao inves de botoes no social

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,20 +6,24 @@ import PaymentInfo from '../../images/paymentInfo.png';
 const Footer = () => {
   return(
     <footer>
-      <h3>VISITE NOSSAS REDES SOCIAIS</h3>
-      <img src={ SocialMedia } alt="informações redes sociais" />
+      <div className="social">
+        <h3>VISITE NOSSAS REDES SOCIAIS</h3>
+        <img src={ SocialMedia } alt="informações redes sociais" />
+      </div>
 
-      <navigator>
+      <nav>
         <a>PERFUME INFANTIL</a>
         <a>SABONETES</a>
         <a>PÓS-BANHO</a>
         <a>ASSADURAS</a>
         <a>HIDRATANTES</a>
         <a>LENÇOS UMEDECIDOS</a>
-      </navigator>
+      </nav>
 
-      <h3>FORMA DE PAGAMENTO</h3>
-      <img src={ PaymentInfo } alt="image payment infos" />
+      <div className="payment">
+        <h3>FORMA DE PAGAMENTO</h3>
+        <img src={ PaymentInfo } alt="image payment infos" />
+      </div>
 
     </footer>
   )

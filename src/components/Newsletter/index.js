59fa6abe55a2d9a5152b8cab65467e4214d98f51
matheustkaczyk <matheus111904@hotmail.com>
2022-02-11 11:03:40 -0300

    Instalado lib axios as dependencias, newsletter fazendo requisicao post com sucesso

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Newsletter = () => {
+  const [input, setInput] = useState('');
+  const [sucess, setSuccess] = useState(false);
+  const handleInput = ({ target }) => {
+    setInput(target.value);
+  };
+
+  const handleSubmit = () => {
+    if (input !== '') {
+      const URL = 'https://foreverliss1.websiteseguro.com/teste-dev/newsletter.php';
+      const data = JSON.stringify({ "email": `${input}` });
+      axios.post(URL, data, {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      })
+        .then((response) => {
+          setSuccess(true);
+        })
+        .catch(() => setSuccess(false));
+    };
+
+    alert('É necessário informar um e-mail');
+    }
+
   return(
     <div className="newsletter">
       <h2>NEWSLETTER FOREVER BABY</h2>
       <h3>Cadastre-se agora para não perder nenhuma promoção</h3>
       <form>
-        <input type="text" placeholder="Digite aqui o seu melhor e-mail"></input>
-        <button type="submit">CADASTRAR</button>
+        <input type="email" onChange={ (e) => handleInput(e) } placeholder="Digite aqui o seu melhor e-mail"></input>
+        <button type="button" onClick={ () => handleSubmit() }>CADASTRAR</button>
       </form>
     </div>
   )

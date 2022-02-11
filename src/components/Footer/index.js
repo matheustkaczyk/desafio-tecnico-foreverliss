a731b0aa34ead69c5c44908e1c883c1a02472a8d
matheustkaczyk <matheus111904@hotmail.com>
2022-02-11 10:45:56 -0300

    trocando a imagem estatica por imagem individual de social, adicionando href aos mesmos

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 
-import SocialMedia from '../../images/socialMedia.png';
 import PaymentInfo from '../../images/paymentInfo.png';
+import socialMediaInsta from '../../images/socialMediaInst.png';
+import socialMediaFace from '../../images/socialMediaFace.png';
+import socialMediaYt from '../../images/socialMediaYt.png';
+import socialMediaTik from '../../images/socialMediaTik.png';
 
 const Footer = () => {
   return(
     <footer>
       <div className="social">
         <h3>VISITE NOSSAS REDES SOCIAIS</h3>
-        <img src={ SocialMedia } alt="informações redes sociais" />
+        <a href="https://www.instagram.com/foreverliss/">
+          <img src={ socialMediaInsta } alt="informações redes sociais" />
+        </a>
+        <a href="https://www.facebook.com/foreverliss">
+          <img src={ socialMediaFace } alt="informações redes sociais" />
+        </a>
+        <a href="https://www.youtube.com/c/ForeverLissOficial">
+          <img src={ socialMediaYt } alt="informações redes sociais" />
+        </a>
+        <a href="https://www.tiktok.com/@foreverlissprofessional">
+          <img src={ socialMediaTik } alt="informações redes sociais" />
+        </a>
       </div>
 
       <nav className="footer-nav">
-        <a>PERFUME INFANTIL</a>
-        <a>SABONETES</a>
-        <a>PÓS-BANHO</a>
-        <a>ASSADURAS</a>
-        <a>HIDRATANTES</a>
-        <a>LENÇOS UMEDECIDOS</a>
+        <a href="/">PERFUME INFANTIL</a>
+        <a href="/">SABONETES</a>
+        <a href="/">PÓS-BANHO</a>
+        <a href="/">ASSADURAS</a>
+        <a href="/">HIDRATANTES</a>
+        <a href="/">LENÇOS UMEDECIDOS</a>
       </nav>
 
       <div className="payment">
